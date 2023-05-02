97454df97a13abdf33402e3556614758d5d44d07
starplanter93 <starplanter93@gmail.com>
2023-05-02 18:58:24 +0900

    generateStaticParams

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -10,4 +10,11 @@ function Pants({ params }: Props) {
   return <div>{params.slug} 제품 설명 페이지</div>;
 }
 
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
+
 export default Pants;

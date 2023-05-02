47de3e5cb31192b759f8b93538b9ee4aa495df33
starplanter93 <starplanter93@gmail.com>
2023-05-02 19:11:14 +0900

    주석 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -6,12 +6,19 @@ type Props = {
   };
 };
 
+//https://nextjs.org/docs/basic-features/pages
+//https://beta.nextjs.org/docs/routing/fundamentals
+//https://beta.nextjs.org/docs/routing/defining-routes
 function Pants({ params }: Props) {
   return <div>{params.slug} 제품 설명 페이지</div>;
 }
 
+//동적 라우팅을 했다 하더라도, 특정 params에 대해서는 ssg를 적용하고 싶을 때, 아래와 같이 generateStaticParams 함수 사용한다.
 export function generateStaticParams() {
+  // ssg를 적용하고 싶은 params를 배열에 추가.
   const products = ["pants", "skirt"];
+
+  // 해당 배열을 mapping 한다. 이때, 각각의 요소들은 동적 라우팅 시 전달받은 params와 같은 형태여야 한다.
   return products.map((product) => ({
     slug: product,
   }));

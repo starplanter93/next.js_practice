bad3a95cb1f55beba8c298a3daefba25b6c95a13
starplanter93 <starplanter93@gmail.com>
2023-05-09 19:01:08 +0900

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-// import { notFound } from "next/navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // notFound();
   return <h1>홈페이지다!!</h1>;
 }

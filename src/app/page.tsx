1e29f9638407dc9bf4826f215e70693b18100dac
starplanter93 <starplanter93@gmail.com>
2023-06-13 17:38:56 +0900

    서버 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import os from 'os'; //노드 api
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('hi');
+  console.log(os.hostname());
   return <h1>홈페이지다!!</h1>;
 }

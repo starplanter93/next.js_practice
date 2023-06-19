a409cc6c0e20b855c0afc719ca451c5ccaf9ac7c
starplanter93 <starplanter93@gmail.com>
2023-06-19 17:21:57 +0900

    fetch로 ISR 설정하기

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,18 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
+import styles from './page.module.css';
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    next: { revalidate: 1 },
+    // cache: 'no-store',
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <div>
       제품 소개 페이지!
@@ -16,6 +23,7 @@ async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <article className={styles.article}>{factText}</article>
     </div>
   );
 }

71564ac74d0472e3fdb548fecf80c706fc1ed380
starplanter93 <starplanter93@gmail.com>
2023-06-19 16:34:16 +0900

    서버 데이터로 ssg 생성하기

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -17,26 +17,24 @@ export function generateMetadata({ params }: Props) {
 //https://nextjs.org/docs/basic-features/pages
 //https://beta.nextjs.org/docs/routing/fundamentals
 //https://beta.nextjs.org/docs/routing/defining-routes
-function Pants({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
 
   if (!product) {
     notFound();
   }
-  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <div>{product} 제품 설명 페이지</div>;
+  return <div>{product.name} 제품 설명 페이지</div>;
 }
 
 //동적 라우팅을 했다 하더라도, 특정 params에 대해서는 ssg를 적용하고 싶을 때, 아래와 같이 generateStaticParams 함수 사용한다.
-export function generateStaticParams() {
+export async function generateStaticParams() {
   // ssg를 적용하고 싶은 params를 배열에 추가.
-  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것임(SSG)
-  const products = getProducts();
+  const products = await getProducts();
 
   // 해당 배열을 mapping 한다. 이때, 각각의 요소들은 동적 라우팅 시 전달받은 params와 같은 형태여야 한다.
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
 
-export default Pants;
+export default ProductPage;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,16 +2,15 @@ import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
 
-function ProductsPage() {
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-  const products = getProducts();
+async function ProductsPage() {
+  const products = await getProducts();
   return (
     <div>
       제품 소개 페이지!
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>

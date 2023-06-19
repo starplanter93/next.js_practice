f257cde730906d4e67741ec4c6ad6dd1fd197c12
starplanter93 <starplanter93@gmail.com>
2023-06-19 15:01:08 +0900

    비동기 구현을 위한 전체적인 틀 잡기

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
+import { getProduct, getProducts } from '@/service/products';
 
 type Props = {
   params: {
@@ -16,17 +17,21 @@ export function generateMetadata({ params }: Props) {
 //https://nextjs.org/docs/basic-features/pages
 //https://beta.nextjs.org/docs/routing/fundamentals
 //https://beta.nextjs.org/docs/routing/defining-routes
-function Pants({ params }: Props) {
-  if (params.slug === 'nothing') {
+function Pants({ params: { slug } }: Props) {
+  const product = getProduct(slug);
+
+  if (!product) {
     notFound();
   }
-  return <div>{params.slug} 제품 설명 페이지</div>;
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <div>{product} 제품 설명 페이지</div>;
 }
 
 //동적 라우팅을 했다 하더라도, 특정 params에 대해서는 ssg를 적용하고 싶을 때, 아래와 같이 generateStaticParams 함수 사용한다.
 export function generateStaticParams() {
   // ssg를 적용하고 싶은 params를 배열에 추가.
-  const products = ['pants', 'skirt'];
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것임(SSG)
+  const products = getProducts();
 
   // 해당 배열을 mapping 한다. 이때, 각각의 요소들은 동적 라우팅 시 전달받은 params와 같은 형태여야 한다.
   return products.map((product) => ({

2e7f101620bad0a5b981a457afb5a0ec60f460ef
starplanter93 <starplanter93@gmail.com>
2023-06-23 16:22:30 +0900

    error 컴포넌트

---
+++ src/app/products/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { useEffect } from 'react';
+
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+
+function ProductsError({ error, reset }: Props) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div>
+      <h2>Something went wrong!</h2>;
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
+
+export default ProductsError;

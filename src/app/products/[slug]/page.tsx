6e902384c3635cdba6d74bb832dffe342832a524
starplanter93 <starplanter93@gmail.com>
2023-06-23 17:18:45 +0900

    제품별 이미지 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 import { getProduct, getProducts } from '@/service/products';
+import Image from 'next/image';
 
 export const revalidate = 3;
 
@@ -25,7 +26,18 @@ async function ProductPage({ params: { slug } }: Props) {
   if (!product) {
     notFound();
   }
-  return <div>{product.name} 제품 설명 페이지</div>;
+
+  return (
+    <>
+      <div>{product.name} 제품 설명 페이지</div>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width='300'
+        height='300'
+      />
+    </>
+  );
 }
 
 //동적 라우팅을 했다 하더라도, 특정 params에 대해서는 ssg를 적용하고 싶을 때, 아래와 같이 generateStaticParams 함수 사용한다.

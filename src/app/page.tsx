16d327a5a89025828a1a92be59e8deec8e178510
starplanter93 <starplanter93@gmail.com>
2023-07-07 18:28:10 +0900

    폰트 설정 및 최적화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
-import { Inter } from "next/font/google";
 import os from 'os'; //노드 api
 import Counter from '@/components/Counter';
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   console.log('hi - 서버');

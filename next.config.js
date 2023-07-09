80fd4c831960cf7c6c86663106d0b6bb72742cc2
starplanter93 <starplanter93@gmail.com>
2023-07-09 18:07:54 +0900

    redirect 설정

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
